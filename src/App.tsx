4b25bd95fae3100cb53e308406094b9f5286534e
Skubenyk <skubenikl@gmail.com>
2023-02-02 12:18:02 +0200

    save localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
         const initialValue = JSON.parse(saved);
         return initialValue;
       }
-      return null;
+      return [];
     }
   );
   const [loading, setLoading] = useState(false);
